047a3a5c4b48d8d45f04e3753c10849a0b4b57a6
DevSamaa <samaakanani@gmail.com>
2019-11-07 14:49:34 +1100

    added authentication to the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @bookings = current_user.bookings
     @rides = current_user.rides.joins(:bookings)
